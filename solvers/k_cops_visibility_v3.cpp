0fd56c35aab1de445ce8e96e2ef1be6e317674ed
lindensheehy <linden.sheehy@outlook.com>
2026-09-21 15:02:23 -0400

    v3 solver first iteration

--- solvers/k_cops_visibility_v3.cpp
+++ solvers/k_cops_visibility_v3.cpp
@@ -1,6 +1,7 @@
 #include "Graph.h"
 #include "AdjacencyList.h"
 #include "AuxGraph.h"
+#include "VertexBitField.h"
 #include "Allocator.h"
 #include "Profiler.h"
 #include "CacheManager.h"
@@ -72,8 +73,8 @@ bool buildAuxGraph() {
 
 bool initializeCaptures() {
 
-    for (size_t cId = 0; cId < aux.configCount; ++cId) {
-        for (int r = 0; r < adj.nodeCount; ++r) {
+    for (size_t cId = 0; cId < aux.configCount; cId++) {
+        for (int r = 0; r < adj.nodeCount; r++) {
 
             DataItem* state = aux.getState(cId, r, 0);
             state->marked = false;
@@ -102,57 +103,159 @@ bool mainLoop() {
         passes++;
 
         // Per node in the column
-        for (size_t cId = 0; cId < aux.configCount; ++cId) {
+        for (size_t cId = 0; cId < aux.configCount; cId++) {
+            for (uint8_t r = 0; r < N; r++) {
 
-            const uint8_t* cops = &aux.configs[cId * k];
-            bool prepared = false;
-            bool skipUnchanged = false;
+                // Skip already marked nodes
+                if (aux.getState(cId, r, 1)->marked) continue;
 
-            struct StackItem {
-                // cop config
-                // robber set
-                // next transition
-            };
+                struct StackItem {
+                    size_t cId;
+                    VertexBitField robberSet;
+                    size_t nextTransition;
+                    size_t lastTransition;
+                };
 
-            // add node to stack
+                std::vector<StackItem> stack;
+                stack.reserve(p);
 
-            int bestDepth = INT_MAX;
+                // Get robber set
+                VertexBitField robberSet;
+                robberSet.set(r);
 
-            // while (stack.size() > 0) {  // stack is not empty
+                // Get range of cop transitions to iterate through
+                size_t firstTransitionOut;
+                size_t lastTransitionOut;
+                aux.getCopTransitions(cId, firstTransitionOut, lastTransitionOut);
 
-                // Read top of stack
+                // Add to stack to start the search from this root
+                stack.push_back({
+                    cId,
+                    robberSet,
+                    firstTransitionOut,
+                    lastTransitionOut
+                });
 
-                // if stack.size() == p*2
-                    // search in one go. for all
+                int bestDepth = INT_MAX;
+
+                while (stack.size() > 0) {
+
+                    // Get top of stack
+                    StackItem& current = stack.back();
+
+                    // If we have iterated over all transitions from the current top of the stack
+                    if (current.nextTransition > current.lastTransition) {
+                        stack.pop_back();
+                        continue;
+                    }
+
+                    // Get next transition from current
+                    size_t nextcId = aux.transitions[current.nextTransition];
+                    current.nextTransition++;
+
+                    VertexBitField newRobberSet = current.robberSet;
+
+                    /*
+                        Simulate cops turn
+
+                        In this step, we let the cops play one possible move from the current tip of the DFS search. This effectively runs the search to one more depth than where the tip last was.
+
+                        Here we trim the last robber set based on the new cop positions.
+
+                        Either:
+                        The new robbers set is empty. In this case, we have found a winning path. We mark the node. We save the depth we are currently at (since it is necessarily <= the current bestDepth), and continue with the rest of the DFS
+                        OR...
+                        The new robbers set is not empty. No win was found on this iteration. We pass through to simulating the robbers turn
+                    */
+                    {
+                        
+                        // Trim robber set
+                        uint8_t* copPositions = &(aux.configs[nextcId]);
+                        for (int i = 0; i < k; i++) {
+                            newRobberSet.reset(copPositions[i]);
+                        }
+
+                        // If the robber set is empty
+                        if (newRobberSet.none()) {
+                            bestDepth = stack.size() * 2;
+                            stack.pop_back();
+                            continue;
+                        }
+                    }
                     
-                    // if forced win
-                        // bestDepth == p*2
-                        // pop from stack
+                    /*
+                        Simulate robbers turn
+                        
+                        Because at this point the robber set must NOT be empty, we then 
+                        simulate the robbers turn.
+                        
+                        This is where we either:
+
+                        Expand the robbers set with all possible next moves (for a column in the middle of the aux graph)
+                        OR...
+                        Play the robbers last invisible turn, and compute the for all in 
+                        the last column transition
+                    */
+                    {
+                        // If its a leaf node (last cop turn column)
+                        // This means the robber becomes now VISIBLE on this 2 ply turn (cop moves, then robber moves, they are now visible)
+                        // We need to check the for all condition, as normal
+                        if (stack.size() == p) {
+
+                            // Search possible robber transitions from this leaf
+                            // If "for all" holds, we can mark this node
+                            
+                            // If we can mark it
+                            {   
+                                // Record this as the best found depth
+                                bestDepth = p * 2; 
+                            }
+                            
+                            // Pop from stack
+                            stack.pop_back();
+
+                            // Go to the next node in the stack
+                            continue;
+                            
+                        // Its not a leaf node (middle column)
+                        // One to one mapping, no "for all" to be done
+                        } else {
+                            
+                            // Expand the robbers set based on reachable nodes from the current set
+
+                        }
+                    }
+
+                    /*
+                        The core DFS loop logic
+
+                        At this point, we know:
+                        - The cops did not catch the robber on this search path (1 turn of cops moving and then robbers moving)
+                        - The robber set is not empty
+                        - The current node does not wrap around to the first column in that range of 2 ply (cops turn and robbers turn)
+
+                        So our job is to take the new node we just found, find the transitions it must iterate over for its own recursive DFS, then push it to the stack
+                    */
+
+                    // If the next depth, AFTER this node we just checked, will be worse than the best depth we found already, we dont add a new node to the stack
+                    if ((stack.size() * 2) + 1 >= bestDepth) {
                         continue;
+                    }
 
-                    // else
-                        // pop from stack
-                        continue;
+                    // Get next transitions
+                    aux.getCopTransitions(nextcId, firstTransitionOut, lastTransitionOut);
 
-                // else
+                    // Push the new node to the stack
+                    stack.push_back({
+                        nextcId,
+                        newRobberSet,
+                        firstTransitionOut,
+                        lastTransitionOut
+                    });
 
-                    // get next transition from current
+                }
 
-                    // trim robber set
-
-                    // if set is empty
-                        // if (currentDepth < bestDepth) bestDepth = currentDepth;
-                        // pop from stack
-                        continue;
-
-                    // if currentDepth >= bestdepth
-                        // pop from stack
-                        continue;
-
-                    // find first transition of new node
-                    // push to stack
-
-            //}
+            }
             
         }
     }
